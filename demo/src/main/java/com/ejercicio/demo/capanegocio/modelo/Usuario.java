52023cdf6b65c366ec73076375e65b44dcdd5dde
mjreb <mjrbcontacto@gmail.com>
2025-08-25 13:47:28 -0600

    proyecto con spring

---
+++ demo/src/main/java/com/ejercicio/demo/capanegocio/modelo/Usuario.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.ejercicio.demo.capanegocio.modelo;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+/**
+ *
+ * @author María de Jesús Rebolledo Bustillo
+ */
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+    
+//    public nombcsdaca
+            
+    
+
+}
